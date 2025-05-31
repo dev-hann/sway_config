811ce8012304c7d84dd8f3dfd328f18c0967518e
Hann (한) <developer.hann@gmail.com>
2025-05-31 10:02:53 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,25 +24,26 @@ cat > ~/.config/sway/config <<EOF
 ### Sway config (customized + default behavior)
 
 # Modifier key
-set \$mod Mod4
+set $mod Mod4
 
 # Applications
-set \$term foot
-set \$menu \"wofi --show drun\"
+set $term foot
+set $menu \"wofi --show drun\"
 
 ### Startup applications
-exec waybar
 exec nm-applet
 exec blueman-applet
 exec mako
 exec swaybg -c '#000000'
 exec fcitx5
+exec waybar
+
 
 ### Key bindings
 
 # Launch apps
-bindsym \$mod+Return exec \$term
-bindsym \$mod+d exec \$menu
+bindsym $mod+Return exec $term
+bindsym $mod+d exec $menu
 
 # Volume control
 bindsym XF86AudioRaiseVolume exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+
@@ -54,73 +55,66 @@ bindsym XF86MonBrightnessUp exec brightnessctl set +10%
 bindsym XF86MonBrightnessDown exec brightnessctl set 10%-
 
 # Kill focused window
-bindsym \$mod+Shift+q kill
+bindsym $mod+Shift+q kill
 
 # Fullscreen toggle
-bindsym \$mod+f fullscreen
+bindsym $mod+f fullscreen
 
 # Focus movement (vim-style)
-bindsym \$mod+h focus left
-bindsym \$mod+j focus down
-bindsym \$mod+k focus up
-bindsym \$mod+l focus right
+bindsym $mod+h focus left
+bindsym $mod+j focus down
+bindsym $mod+k focus up
+bindsym $mod+l focus right
 
 # Move windows
-bindsym \$mod+Shift+h move left
-bindsym \$mod+Shift+j move down
-bindsym \$mod+Shift+k move up
-bindsym \$mod+Shift+l move right
+bindsym $mod+Shift+h move left
+bindsym $mod+Shift+j move down
+bindsym $mod+Shift+k move up
+bindsym $mod+Shift+l move right
 
 # Workspace switching
-bindsym \$mod+1 workspace 1
-bindsym \$mod+2 workspace 2
-bindsym \$mod+3 workspace 3
-bindsym \$mod+4 workspace 4
-bindsym \$mod+5 workspace 5
-bindsym \$mod+6 workspace 6
-bindsym \$mod+7 workspace 7
-bindsym \$mod+8 workspace 8
-bindsym \$mod+9 workspace 9
+bindsym $mod+1 workspace 1
+bindsym $mod+2 workspace 2
+bindsym $mod+3 workspace 3
+bindsym $mod+4 workspace 4
+bindsym $mod+5 workspace 5
+bindsym $mod+6 workspace 6
+bindsym $mod+7 workspace 7
+bindsym $mod+8 workspace 8
+bindsym $mod+9 workspace 9
 
 # Move focused container to workspace
-bindsym \$mod+Shift+1 move container to workspace 1
-bindsym \$mod+Shift+2 move container to workspace 2
-bindsym \$mod+Shift+3 move container to workspace 3
-bindsym \$mod+Shift+4 move container to workspace 4
-bindsym \$mod+Shift+5 move container to workspace 5
-bindsym \$mod+Shift+6 move container to workspace 6
-bindsym \$mod+Shift+7 move container to workspace 7
-bindsym \$mod+Shift+8 move container to workspace 8
-bindsym \$mod+Shift+9 move container to workspace 9
+bindsym $mod+Shift+1 move container to workspace 1
+bindsym $mod+Shift+2 move container to workspace 2
+bindsym $mod+Shift+3 move container to workspace 3
+bindsym $mod+Shift+4 move container to workspace 4
+bindsym $mod+Shift+5 move container to workspace 5
+bindsym $mod+Shift+6 move container to workspace 6
+bindsym $mod+Shift+7 move container to workspace 7
+bindsym $mod+Shift+8 move container to workspace 8
+bindsym $mod+Shift+9 move container to workspace 9
 
 # Toggle floating
-bindsym \$mod+Shift+space floating toggle
+bindsym $mod+Shift+space floating toggle
 
 # Reload/Restart/Exit sway
-bindsym \$mod+Shift+c reload
-bindsym \$mod+Shift+r restart
-bindsym \$mod+Shift+e exit
+bindsym $mod+Shift+c reload
+bindsym $mod+Shift+r restart
+bindsym $mod+Shift+e exit
 
 # Change layout mode
-bindsym \$mod+s layout stacking
-bindsym \$mod+w layout tabbed
-bindsym \$mod+e layout toggle split
-
-# Gaps (optional, sway doesn't support gaps by default unless patched)
-# bindsym \$mod+g gaps inner current plus 5
+bindsym $mod+s layout stacking
+bindsym $mod+w layout tabbed
+bindsym $mod+e layout toggle split
 
 ### Input (keyboard, touchpad, etc.)
 input type:keyboard {
   xkb_layout us,kr
-  xkb_options grp:alt_shift_toggle
 }
 
 input "type:touchpad" {
   natural_scroll enabled
   tap enabled
-}
-bar {
- swaybar_command waybar
 }
 
 EOF
